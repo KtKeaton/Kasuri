f3c46e56ca8e71f909499255463c794923a04607
KtKeaton <ktr01@protonmail.com>
2021-12-11 14:54:08 +0800

    WIP 登入系統Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :managers
-  devise_for :users
+  devise_for :users, controllers: {sessions: 'users/sessions'}
+  devise_for :managers, controllers: {sessions: 'managers/sessions'}
+
   root to: "welcome#index"
+
 end
